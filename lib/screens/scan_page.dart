12a9aac3816a5258e135ff13e6c7250a0819de05
PrusWielki <prusakpat@gmail.com>
2026-01-15 18:34:17 +0100

    fix: QR code scanning multiple times and causing black screen

--- lib/screens/scan_page.dart
+++ lib/screens/scan_page.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
-class QRScanPage extends StatelessWidget {
+class QRScanPage extends StatefulWidget {
   final Function(String) onResult;
   const QRScanPage({super.key, required this.onResult});
+
+  @override
+  State<QRScanPage> createState() => _QRScanPageState();
+}
+
+class _QRScanPageState extends State<QRScanPage> {
+  bool _isScanned = false;
 
   @override
   Widget build(BuildContext context) {
@@ -11,10 +18,14 @@ class QRScanPage extends StatelessWidget {
       appBar: AppBar(title: const Text('Scan QR Code')),
       body: MobileScanner(
         onDetect: (capture) {
+          if (_isScanned) return;
           final List<Barcode> barcodes = capture.barcodes;
           for (final barcode in barcodes) {
             if (barcode.rawValue != null) {
-              onResult(barcode.rawValue!);
+              setState(() {
+                _isScanned = true;
+              });
+              widget.onResult(barcode.rawValue!);
               Navigator.pop(context);
               break;
             }
